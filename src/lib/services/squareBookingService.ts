6950ccccdc042e7df8d931c24f0a3b9f75ded520
Mason Leitch <nosamleitch@gmail.com>
2025-03-30 16:54:25 -0700

    Type errors

--- src/lib/services/squareBookingService.ts
+++ src/lib/services/squareBookingService.ts
@@ -124,6 +124,7 @@ interface CreateAppointmentParams {
     clientEmail: string;
     clientPhone: string;
     serviceId: string;
+    variationVersion: string;
     staffId: string;
     startTime: string;
     endTime: string;
